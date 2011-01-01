57f9115d17faa2af416d86a2b6307c60797f7eef
Jörg Battermann <jb@joergbattermann.com>
2011-01-01 17:11:27 +0100

    Added sample app skeleton

---
+++ simplegeo.Net.SampleApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,27 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Jörg Battermann">
+//   Copyright © Jörg Battermann 2011
+// </copyright>
+// <summary>
+//   AssemblyInfo.cs
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("simplegeo.Net.Tests")]
+[assembly: AssemblyDescription("Unit tests for the simplegeo.Net library")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Jörg Battermann")]
+[assembly: AssemblyProduct("simplegeo.Net.Tests")]
+[assembly: AssemblyCopyright("Copyright © Jörg Battermann 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("0.0.0.1")]
+
+
+[assembly: Guid("34b6c1a3-b891-4bed-8b9d-9cb382764c0c")]
