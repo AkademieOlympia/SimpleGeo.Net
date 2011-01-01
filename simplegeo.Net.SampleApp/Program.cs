a0b0ce72ffda29f5811915ba00d48abe5f80e19e
Jörg Battermann <jb@joergbattermann.com>
2011-01-01 20:32:10 +0100

    using hammock .. for some reason RestSharp does not work well at first sight and do not want to keep fiddling for now

--- simplegeo.Net.SampleApp/Program.cs
+++ simplegeo.Net.SampleApp/Program.cs
@@ -5,10 +5,42 @@ using System.Text;
 
 namespace simplegeo.Net.SampleApp
 {
+    using Hammock;
+    using Hammock.Authentication.OAuth;
+    
     class Program
     {
         static void Main(string[] args)
         {
+            string consumerKey = "<enter here>";
+            string consumerSecret = "<enter here>";
+            
+            var baseUrl = "http://api.simplegeo.com";
+            var apiVersion = "1.0";           
+            var apiUrl = string.Format("{0}/{1}/", baseUrl, apiVersion);
+            var userAgent = "simplegeo.Net Client Library";
+
+            var credentials = new OAuthCredentials
+            {
+                Type = OAuthType.RequestToken,
+                SignatureMethod = OAuthSignatureMethod.HmacSha1,
+                ParameterHandling = OAuthParameterHandling.HttpAuthorizationHeader,
+                ConsumerKey = consumerKey,
+                ConsumerSecret = consumerSecret,
+            };
+
+            var client = new RestClient
+            {
+                Authority = apiUrl,
+                Credentials = credentials,
+            };
+
+            var request = new RestRequest { Path = "context/address.json" };
+            request.AddParameter("address", "San Francisco, CA");
+
+            var response = client.Request(request);
+
+            Console.Write(response);
         }
     }
 }
