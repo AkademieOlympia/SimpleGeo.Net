060e945e8e2cd8dd5682407dbef30accb5c3582a
Jörg Battermann <jb@joergbattermann.com>
2011-01-01 16:34:38 +0100

    Here we go

---
+++ simplegeo.Net/Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Jörg Battermann">
+//   Copyright © Jörg Battermann 2011
+// </copyright>
+// <summary>
+//   AssemblyInfo.cs
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("simplegeo.Net")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Jörg Battermann")]
+[assembly: AssemblyProduct("simplegeo.Net")]
+[assembly: AssemblyCopyright("Copyright © Jörg Battermann 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0fb2bfab-dceb-4590-aa55-270f6e2a8bb0")]
+
+[assembly: AssemblyVersion("0.0.0.1")]
