8bdac14ac67f0b9905c2d268b1a9987e355c8c6e
John Murray <john.murray@mavizontech.com>
2011-05-20 08:28:59 -0400

    added attributions and helpers

---
+++ SimpleGeo.Net/Context.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleGeo.Net
+{
+    class Context
+    {
+        // TODO: this may only work for lon,lat requests
+        private KeyValuePair<String, Double>[] query = new KeyValuePair<String, Double>[2];
+        private String timestamp;
+        private FeatureCollection Features;
+
+    }
+}
